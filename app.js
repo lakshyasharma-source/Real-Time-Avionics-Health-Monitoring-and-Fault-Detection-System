e45de96d168b7255660082c8025f7c42b83a9943
lakshyasharma-source <61679848+lakshyasharma-source@users.noreply.github.com>
2025-06-13 16:32:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './Dashboard'; // Adjust the path if Dashboard.js is in a subfolder
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
